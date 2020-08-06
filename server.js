23ce6d0fca5756dc006cedc839605a47939d0323
K.Niroshani <niroshini.07.skk@gmail.com>
2020-08-06 20:18:27 +0530

    Initialize first view

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express(); //Initialize express
 const server = require("http").Server(app);
 
+//set ejs as view engine
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+  //   res.status(200).send("Hello World!");
+  res.render("room");
 });
 
 server.listen(3030);
