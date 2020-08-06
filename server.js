58044c5faa5fd998c26cb958c5edaaf1994a70fc
K.Niroshani <niroshini.07.skk@gmail.com>
2020-08-06 21:52:31 +0530

    ADD: get video function, style video grid

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express(); //Initialize express
 const server = require("http").Server(app);
-
-//set ejs as view engine
-app.set("view engine", "ejs");
+const { v4: uuidv4 } = require("uuid"); //Import the uuid library
+app.set("view engine", "ejs"); //set ejs as view engine
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   //   res.status(200).send("Hello World!");
-  res.render("room");
+  //   res.render("room");
+  res.redirect(`/${uuidv4()}`); // Redirect root URLgenerate unique uuid
 });
 
+//new Url to represent room ID
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 server.listen(3030);
